68ce5c3eca139599c8f55879bb2c536bfde559d5
maxlajoie99 <max.lajoie99@hotmail.com>
2018-07-18 13:44:56 -0400

    Depth Testing - Fragment color basé sur le Depth Linéaire

--- src/main/resources/ca/cidco/opengl/shaders/fragment/depthshader.fs
+++ src/main/resources/ca/cidco/opengl/shaders/fragment/depthshader.fs
@@ -3,10 +3,21 @@ out vec4 FragColor;
 
 in vec2 TexCoords;
 
+float near = 0.1;
+float far = 100.0;
+
 uniform sampler2D texture1;
+
+float LinearizeDepth(float depth)
+{
+	float z = depth * 2.0 - 1.0;
+	float linearDepth = (2.0 * near * far)/(far + near - z * (far - near));
+	return linearDepth;
+}
 
 void main()
 {    
+	float depth = LinearizeDepth(gl_FragCoord.z)/far;
     //FragColor = texture(texture1, TexCoords);
-	FragColor = vec4(vec3(gl_FragCoord.z), 1.0);
+	FragColor = vec4(vec3(depth), 1.0);
 }

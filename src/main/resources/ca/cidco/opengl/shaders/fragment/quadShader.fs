e7349b01e45979a2ab091cb56c920595345745ca
maxlajoie99 <max.lajoie99@hotmail.com>
2018-07-20 12:18:34 -0400

    Framebuffers - PostProcessing - Kernels - Sharpen

--- src/main/resources/ca/cidco/opengl/shaders/fragment/quadShader.fs
+++ src/main/resources/ca/cidco/opengl/shaders/fragment/quadShader.fs
@@ -6,9 +6,37 @@ in vec2 TexCoords;
 
 uniform sampler2D texture1;
 
+const float offset = 1.0/300.0;
+
 void main()
 {
-    FragColor = texture(texture1, TexCoords);
-	float average = 0.2126 * FragColor.x + 0.7152 * FragColor.y + 0.0722 * FragColor.z;
-	FragColor = vec4(vec3(average), 1.0);
+	vec2 offsets[9] = vec2[](
+		vec2(-offset, offset),
+		vec2(0.0, offset),
+		vec2(offset, offset),
+		vec2(-offset, 0.0),
+		vec2(0.0, 0.0),
+		vec2(offset, 0.0),
+		vec2(-offset, -offset),
+		vec2(0.0, -offset),
+		vec2(offset, -offset)
+	);
+
+	float kernel[9] = float[](
+		-1, -1, -1,
+		-1,  9, -1,
+		-1, -1, -1
+	);
+
+	vec3 sampleTex[9];
+	for(int i = 0; i < 9; i++)
+	{
+		sampleTex[i] = vec3(texture(texture1, TexCoords.st + offsets[i]));
+	}
+
+	vec3 col = vec3(0.0);
+	for(int i = 0; i < 9; i++)
+		col += sampleTex[i] * kernel[i];
+
+	FragColor = vec4(col, 1.0);
 }
